2303fa79b4dd7a9eb9bbc6506cedc128de7841c2
JulesRumeauoui <102958406+JulesRumeauoui@users.noreply.github.com>
2022-11-12 10:39:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+
+var premiermdpok = false;
 function getValue() {
     // Sélectionner l'élément input et récupérer sa valeur
     var input = document.getElementById("in").value;
 
     hash(input).then((hex) => test(hex));
+
+    //console.log(premiermdpok);
+}
+
+function getValue2() {
+  // Sélectionner l'élément input et récupérer sa valeur
+  var input = document.getElementById("in").value;
+
+  hash(input).then((hex) => test2(hex));
+
+  //console.log(premiermdpok);
 }
 
 
@@ -23,7 +36,23 @@ function hash(string) {
     var sha = "10bc0289455880c6162d632c9d9a2edaec2f7182cf952608cee965ac8d623d14";
     
     if (sha == lehash){
-        document.getElementById('reponse').textContent = 'Ok c\'est bon';
+        //document.getElementById('reponse').textContent = 'Ok c\'est bon';
+        document.getElementById('lien').textContent = 'Ok c\'est bon';
+
+        premiermdpok = true;
+    } else {
+        document.getElementById('reponse').textContent = 'C\'est pas ça';
+    }
+  }
+
+  function test2(lehash) {
+    
+    var sha = "77173ace11f118f4cfaaa33d7c35ec612a1ca67dae67d54a4a677a465124b087";
+    
+    if (sha == lehash){
+        document.getElementById('reponse').textContent = '@marcosorntino';
+
+        premiermdpok = true;
     } else {
         document.getElementById('reponse').textContent = 'C\'est pas ça';
     }
